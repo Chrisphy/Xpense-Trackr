b21fd92c46aeaae68cb2f51e98c04dad21778745
CHRIS CHONG <chcho@l-b-he1021-01.du.deakin.edu.au>
2017-05-05 17:35:59 +1000

    Second commit, table views

---
+++ Xpense Trackr/ExpenseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExpenseTableViewCell.swift
+//  Xpense Trackr
+//
+//  Created by CHRIS CHONG on 5/05/2017.
+//  Copyright © 2017 CHRIS CHONG. All rights reserved.
+//
+
+import UIKit
+
+class ExpenseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
